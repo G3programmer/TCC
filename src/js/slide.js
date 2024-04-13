443f22f9985cee951e5f251cd420ae94df5987f0
G_03 <130713943+G3programmer@users.noreply.github.com>
2024-04-13 15:59:32 -0300

    Add files via upload

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,42 +1,17 @@
-window.onload = function () {
-    const slideContainer = document.querySelector('.slide');
-    const slides = document.querySelectorAll('.slide img');
-    const slideIndicatorsContainer = document.querySelector('.slide-indicators');
-    let currentSlide = 0;
+let count = 1;
+document.getElementById("radio1").checked = true;
 
-    const showSlide = (index) => {
-        slideContainer.style.transition = 'transform 1s ease';
-        slideContainer.style.transform = `translateX(-${index * 100}%)`;
-        updateIndicators(index);
-    };
+setInterval(function () {
+    nextImage();
+}, 9000)
 
-    const updateIndicators = (index) => {
-        const indicators = document.querySelectorAll('.slide-indicator');
-        indicators.forEach((indicator, i) => {
-            if (i === index) {
-                indicator.classList.add('active');
-            } else {
-                indicator.classList.remove('active');
-            }
-        });
-    };
+function nextImage() {
+    count++;
+    if (count > 3) {
+        count = 1;
 
-    // Criando indicadores para cada slide
-    slides.forEach((slide, index) => {
-        const indicator = document.createElement('div');
-        indicator.classList.add('slide-indicator');
-        if (index === 0) indicator.classList.add('active');
-        indicator.addEventListener('click', () => {
-            showSlide(index);
-        });
-        slideIndicatorsContainer.appendChild(indicator);
-    });
+    }
 
-    setInterval(() => {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-    }, 5000); // Altera o slide a cada 5 segundos
+    document.getElementById("radio" + count).checked = true;
 
-    // Exibir o primeiro slide ao carregar a página
-    showSlide(currentSlide);
-};
+}

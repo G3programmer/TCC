063059842eb6502467acfcec2e1f8b1fef827f39
G_03 <130713943+G3programmer@users.noreply.github.com>
2024-09-15 22:02:32 -0300

    15.09

--- src/php/pesquisaProdutos.php
+++ src/php/pesquisaProdutos.php
@@ -1,11 +1,11 @@
 <?php
-include ('index.php');
+include ('conexao.php');
 
 if (!isset($_GET['busca'])) {
     echo "Nenhum termo de busca foi fornecido.";
 } else {
     $pesquisa = $conn->real_escape_string($_GET['busca']);
-    $sql_code = "SELECT * FROM produto WHERE nome LIKE '%$pesquisa%' OR classe LIKE '%$pesquisa%'";
+    $sql_code = "SELECT * FROM produtos WHERE nome_produto LIKE '%$pesquisa%' OR classe LIKE '%$pesquisa%'";
     $sql_query = $conn->query($sql_code) or die("ERRO ao consultar! " . $conn->error); 
     
 
@@ -22,9 +22,9 @@ if (!isset($_GET['busca'])) {
         <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/css/index-produto.css">
+    <link rel="stylesheet" href="../css/index-produto.css">
     <link rel="stylesheet" href="../css/style-produtos.css">
-    <link rel="shortcut icon" href="src/imagem/icones/escudo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../imagem/icones/escudo.png" type="image/x-icon">
     ">
     <link href="https://fonts.cdnfonts.com/css/milestone-one" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/codygoon" rel="stylesheet">
@@ -37,8 +37,7 @@ if (!isset($_GET['busca'])) {
                 <input type="text" name="busca" class="barra" placeholder="Busco por...">
                 <button type="submit" class="pesquisa-btn">
                     <img src="src/imagem/icones/lupa-azul.png" alt="" class="lupa-azul" width="25px" height="25px">
-                    <img src="src/imagem/icones/lupa-Branca.png" alt="" class="lupa-branca" width="25px" height="25px">
-                    </a>
+                    </button>
             </div>
         </form>
         <h1 class="titulo">Nossos Produtos</h1>
@@ -62,13 +61,12 @@ if ($sql_query->num_rows == 0) {
         </div>    
 
        <h1 class="titulo">Sistemas Operacionais</h1>                        
-                            <div class="lista">                
-                                <button class="prev" onclick="prevSlide()">&#10094;</button>
+                            <div class="lista">     
                                 <div class="cartao">
                 <div class="informacoes"> 
                 <img src="data:image/jpeg;base64,<?= base64_encode($binary) ?>" alt="Imagem do produto" class="imagem" />
 
-                <h3 class="titulo"><?php echo $dados['nome']; ?></h3>
+                <h3 class="titulo"><?php echo $dados['nome_produto']; ?></h3>
 
                     <p class="texto"><?php echo $dados['descricao']; ?></p>
                      
